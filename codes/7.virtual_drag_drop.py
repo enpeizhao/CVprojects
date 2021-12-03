9f3c57b6bb0bf51fb2d0595b9bcd0e34445879a5
zhangjie <zhangjie1493@163.com>
2021-12-03 14:41:36 +0800

    添加点击叉号结束运行的功能

--- codes/7.virtual_drag_drop.py
+++ codes/7.virtual_drag_drop.py
@@ -10,7 +10,6 @@
 5、两指放开，则矩形停止移动
 """
 
-
 # 导入OpenCV
 import cv2
 # 导入mediapipe
@@ -21,16 +20,13 @@ import time
 import math
 
 
-
-
 # 方块管理类
 class SquareManager:
-    def __init__(self,rect_width):
-        
-        
+    def __init__(self, rect_width):
+
         # 方框长度
         self.rect_width = rect_width
-        
+
         # 方块list
         self.square_count = 0
         self.rect_left_x_list = []
@@ -46,60 +42,55 @@ class SquareManager:
 
         # 激活的方块ID
         self.active_index = -1
-        
-    
+
     # 创建一个方块，但是没有显示
-    def create(self,rect_left_x,rect_left_y,alpha=0.4):
+    def create(self, rect_left_x, rect_left_y, alpha=0.4):
         self.rect_left_x_list.append(rect_left_x)
         self.rect_left_y_list.append(rect_left_y)
         self.alpha_list.append(alpha)
-        self.square_count+=1
-        
+        self.square_count += 1
 
     # 更新位置
-    def display(self,class_obj):
-        for i in range(0,self.square_count):
-            x= self.rect_left_x_list[i]
-            y= self.rect_left_y_list[i]
-            alpha  = self.alpha_list[i]
+    def display(self, class_obj):
+        for i in range(0, self.square_count):
+            x = self.rect_left_x_list[i]
+            y = self.rect_left_y_list[i]
+            alpha = self.alpha_list[i]
 
             overlay = class_obj.image.copy()
 
-            if(i == self.active_index):
-                cv2.rectangle(overlay,(x,y),(x+self.rect_width,y+self.rect_width),(255, 0, 255),-1)
+            if (i == self.active_index):
+                cv2.rectangle(overlay, (x, y), (x + self.rect_width, y + self.rect_width), (255, 0, 255), -1)
             else:
-                cv2.rectangle(overlay,(x,y),(x+self.rect_width,y+self.rect_width),(255, 0, 0),-1)
-            
+                cv2.rectangle(overlay, (x, y), (x + self.rect_width, y + self.rect_width), (255, 0, 0), -1)
+
             # Following line overlays transparent rectangle over the self.image
             class_obj.image = cv2.addWeighted(overlay, alpha, class_obj.image, 1 - alpha, 0)
-            
-
 
     # 判断落在哪个方块上，返回方块的ID
-    def checkOverlay(self,check_x,check_y):
-        for i in range(0,self.square_count):
-            x= self.rect_left_x_list[i]
-            y= self.rect_left_y_list[i]
+    def checkOverlay(self, check_x, check_y):
+        for i in range(0, self.square_count):
+            x = self.rect_left_x_list[i]
+            y = self.rect_left_y_list[i]
 
-            if (x <  check_x < (x+self.rect_width) ) and ( y < check_y < (y+self.rect_width)):
-                
+            if (x < check_x < (x + self.rect_width)) and (y < check_y < (y + self.rect_width)):
                 # 保存被激活的方块ID
                 self.active_index = i
 
                 return i
-        
+
         return -1
 
     # 计算与指尖的距离
-    def setLen(self,check_x,check_y):
+    def setLen(self, check_x, check_y):
         # 计算距离
-        self.L1 = check_x - self.rect_left_x_list[self.active_index] 
+        self.L1 = check_x - self.rect_left_x_list[self.active_index]
         self.L2 = check_y - self.rect_left_y_list[self.active_index]
 
     # 更新方块    
-    def updateSquare(self,new_x,new_y):
+    def updateSquare(self, new_x, new_y):
         # print(self.rect_left_x_list[self.active_index])
-        self.rect_left_x_list[self.active_index] = new_x - self.L1 
+        self.rect_left_x_list[self.active_index] = new_x - self.L1
         self.rect_left_y_list[self.active_index] = new_y - self.L2
 
 
@@ -111,15 +102,12 @@ class HandControlVolume:
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_hands = mp.solutions.hands
 
-        
         # 中指与矩形左上角点的距离
         self.L1 = 0
         self.L2 = 0
 
         # image实例，以便另一个类调用
-        self.image=None
-
-        
+        self.image = None
 
     # 主函数
     def recognize(self):
@@ -135,14 +123,12 @@ class HandControlVolume:
         # 画面显示初始化参数
         rect_percent_text = 0
 
-
         # 初始化方块管理器
         squareManager = SquareManager(150)
 
         # 创建多个方块
-        for i in range(0,5):
-            squareManager.create(200*i+20,200,0.6)
-
+        for i in range(0, 5):
+            squareManager.create(200 * i + 20, 200, 0.6)
 
         with self.mp_hands.Hands(min_detection_confidence=0.7,
                                  min_tracking_confidence=0.5,
@@ -180,7 +166,6 @@ class HandControlVolume:
                             self.mp_drawing_styles.get_default_hand_landmarks_style(),
                             self.mp_drawing_styles.get_default_hand_connections_style())
 
-
                         # 解析手指，存入各个手指坐标
                         landmark_list = []
 
@@ -199,83 +184,89 @@ class HandControlVolume:
                             # 比例缩放到像素
                             ratio_x_to_pixel = lambda x: math.ceil(x * resize_w)
                             ratio_y_to_pixel = lambda y: math.ceil(y * resize_h)
-                            
+
                             # 设计手掌左上角、右下角坐标
-                            paw_left_top_x,paw_right_bottom_x = map(ratio_x_to_pixel,[min(paw_x_list),max(paw_x_list)])
-                            paw_left_top_y,paw_right_bottom_y = map(ratio_y_to_pixel,[min(paw_y_list),max(paw_y_list)])
+                            paw_left_top_x, paw_right_bottom_x = map(ratio_x_to_pixel,
+                                                                     [min(paw_x_list), max(paw_x_list)])
+                            paw_left_top_y, paw_right_bottom_y = map(ratio_y_to_pixel,
+                                                                     [min(paw_y_list), max(paw_y_list)])
 
                             # 给手掌画框框
-                            cv2.rectangle(self.image,(paw_left_top_x-30,paw_left_top_y-30),(paw_right_bottom_x+30,paw_right_bottom_y+30),(0, 255,0),2)
+                            cv2.rectangle(self.image, (paw_left_top_x - 30, paw_left_top_y - 30),
+                                          (paw_right_bottom_x + 30, paw_right_bottom_y + 30), (0, 255, 0), 2)
 
                             # 获取中指指尖坐标
                             middle_finger_tip = landmark_list[12]
-                            middle_finger_tip_x =ratio_x_to_pixel(middle_finger_tip[1])
-                            middle_finger_tip_y =ratio_y_to_pixel(middle_finger_tip[2])
+                            middle_finger_tip_x = ratio_x_to_pixel(middle_finger_tip[1])
+                            middle_finger_tip_y = ratio_y_to_pixel(middle_finger_tip[2])
 
                             # 获取食指指尖坐标
                             index_finger_tip = landmark_list[8]
-                            index_finger_tip_x =ratio_x_to_pixel(index_finger_tip[1])
-                            index_finger_tip_y =ratio_y_to_pixel(index_finger_tip[2])
+                            index_finger_tip_x = ratio_x_to_pixel(index_finger_tip[1])
+                            index_finger_tip_y = ratio_y_to_pixel(index_finger_tip[2])
                             # 中间点
-                            between_finger_tip = (middle_finger_tip_x+index_finger_tip_x)//2, (middle_finger_tip_y+index_finger_tip_y)//2
+                            between_finger_tip = (middle_finger_tip_x + index_finger_tip_x) // 2, (
+                                        middle_finger_tip_y + index_finger_tip_y) // 2
                             # print(middle_finger_tip_x)
-                            thumb_finger_point = (middle_finger_tip_x,middle_finger_tip_y)
-                            index_finger_point = (index_finger_tip_x,index_finger_tip_y)
+                            thumb_finger_point = (middle_finger_tip_x, middle_finger_tip_y)
+                            index_finger_point = (index_finger_tip_x, index_finger_tip_y)
                             # 画指尖2点
-                            circle_func = lambda point: cv2.circle(self.image,point,10,(255,0,255),-1)
+                            circle_func = lambda point: cv2.circle(self.image, point, 10, (255, 0, 255), -1)
                             self.image = circle_func(thumb_finger_point)
                             self.image = circle_func(index_finger_point)
                             self.image = circle_func(between_finger_tip)
                             # 画2点连线
-                            self.image = cv2.line(self.image,thumb_finger_point,index_finger_point,(255,0,255),5)
+                            self.image = cv2.line(self.image, thumb_finger_point, index_finger_point, (255, 0, 255), 5)
                             # 勾股定理计算长度
-                            line_len = math.hypot((index_finger_tip_x-middle_finger_tip_x),(index_finger_tip_y-middle_finger_tip_y))
+                            line_len = math.hypot((index_finger_tip_x - middle_finger_tip_x),
+                                                  (index_finger_tip_y - middle_finger_tip_y))
                             # 将指尖距离映射到文字
                             rect_percent_text = math.ceil(line_len)
 
                             # 激活模式，需要让矩形跟随移动
                             if squareManager.drag_active:
                                 # 更新方块
-                                squareManager.updateSquare(between_finger_tip[0],between_finger_tip[1])
-                                if(line_len>100):
+                                squareManager.updateSquare(between_finger_tip[0], between_finger_tip[1])
+                                if (line_len > 100):
                                     # 取消激活
-                                    squareManager.drag_active =False
+                                    squareManager.drag_active = False
                                     squareManager.active_index = -1
 
-                            elif (line_len<100) and (squareManager.checkOverlay(between_finger_tip[0],between_finger_tip[1]) != -1 )and( squareManager.drag_active  == False):
-                                    # 激活
-                                    squareManager.drag_active =True
-                                    # 计算距离
-                                    squareManager.setLen(between_finger_tip[0],between_finger_tip[1])
+                            elif (line_len < 100) and (squareManager.checkOverlay(between_finger_tip[0],
+                                                                                  between_finger_tip[1]) != -1) and (
+                                    squareManager.drag_active == False):
+                                # 激活
+                                squareManager.drag_active = True
+                                # 计算距离
+                                squareManager.setLen(between_finger_tip[0], between_finger_tip[1])
 
                 # 显示方块，传入本实例，主要为了半透明的处理
                 squareManager.display(self)
-                
+
                 # 显示距离
-                cv2.putText(self.image, "Distance:"+str(rect_percent_text), (10, 120),cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+                cv2.putText(self.image, "Distance:" + str(rect_percent_text), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
+                            (255, 0, 0), 3)
 
                 # 显示当前激活
-                cv2.putText(self.image, "Active:"+( "None" if squareManager.active_index == -1 else str(squareManager.active_index)), (10, 170),cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+                cv2.putText(self.image, "Active:" + (
+                    "None" if squareManager.active_index == -1 else str(squareManager.active_index)), (10, 170),
+                            cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
                 # 显示刷新率FPS
                 cTime = time.time()
-                fps_text = 1/(cTime-fpsTime)
+                fps_text = 1 / (cTime - fpsTime)
                 fpsTime = cTime
                 cv2.putText(self.image, "FPS: " + str(int(fps_text)), (10, 70),
                             cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
                 # 显示画面
                 # self.image = cv2.resize(self.image, (resize_w//2, resize_h//2))
                 cv2.imshow('virtual drag and drop', self.image)
-                
-                if cv2.waitKey(5) & 0xFF == 27:
+
+                if cv2.waitKey(5) & 0xFF == 27 or cv2.getWindowProperty('virtual drag and drop', cv2.WND_PROP_VISIBLE) < 1:
                     break
             cap.release()
-
 
 
 # 开始程序
 control = HandControlVolume()
 control.recognize()
-
-
-
